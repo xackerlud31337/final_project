e2cdd25fe009b48d2a0366d0c6c37ca5ac833022
xackerlud31337 <i.mandev@student.utwente.nl>
2025-06-25 16:51:33 +0200

    fixed

--- stack-my-lang/src/MyCodeGen.hs
+++ stack-my-lang/src/MyCodeGen.hs
@@ -1,10 +1,32 @@
 module MyCodeGen
     ( codeGen ) where
 
-import MyParser (Stmt)
 import Sprockell
+import MyParser (Stmt(..), Expr(..))
 
+-- Entry point
 codeGen :: [Stmt] -> [Instruction]
-codeGen n = [ 
-       
-       ]
+codeGen stmts = concatMap compileStmt stmts ++ [EndProg]
+
+-- Compile a statement into Sprockell instructions
+compileStmt :: Stmt -> [Instruction]
+compileStmt stmt = case stmt of
+    Print e       -> compileExpr e ++ [WriteInstr regA numberIO]
+    Assign _ _    -> [] -- placeholder
+    Decl _ _      -> [] -- placeholder
+    If _ _ _      -> [] -- not implemented yet
+    While _ _     -> [] -- not implemented yet
+    ForkJoin _    -> [] -- not implemented yet
+    Lock _ _      -> [] -- not implemented yet
+    Block ss      -> concatMap compileStmt ss
+
+-- Compile an expression into Sprockell instructions
+-- Result will be in regA
+compileExpr :: Expr -> [Instruction]
+compileExpr expr = case expr of
+    IntLit n        -> [Load (ImmValue $ fromInteger n) regA]
+    BinOp "+" l r   -> compileBinary l r Add
+    BinOp "-" l r   -> compileBinary l r Sub
+    BinOp "*" l r   -> compileBinary l r Mul
+    BinOp "/" l r   -> compileBinary l r Div
+    _               -> [NOP] -- unsupported expr
