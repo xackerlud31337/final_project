9525c2cdfb54f0a8c7ec9856bcbe056fa55f6f41
ValWalker0304 <gyunderov@gmail.com>
2025-07-03 13:12:14 +0200

    Refactoring the CodeGen
    Absolute cinema

--- stack-my-lang/app/Main.hs
+++ stack-my-lang/app/Main.hs
@@ -1,20 +1,16 @@
 module Main where
 
 import MyParser (parseMyLang)
-import MyCodeGen (codeGen)
+import MyCodeGen (codeGen, prog)
 import Sprockell (run, Instruction)
 
 -- Compiles a number into a spril program producing all fibonacci numbers below the number
 -- Compilation might fail
 compile :: String -> Either String [Instruction]
-compile txt = do
-    ast <- parseMyLang txt
-    pure $ codeGen ast
+case parseMyLang src of
+    Left err    -> error err
+    Right stmts -> let (instrs, _, _) = compileStmtsWithLabels regPool 0 stmts in instrs ++ [EndProg]
 
 -- Gets a number and runs the resulting spril program of compilation succeeds
 main :: IO ()
-main = do
-    txt <- getLine
-    case compile txt of
-        (Left err) -> fail err
-        (Right spril) -> run [spril]
+main = run [prog]
--- stack-my-lang/src/MyCodeGen.hs
+++ stack-my-lang/src/MyCodeGen.hs
@@ -1,10 +1,53 @@
 module MyCodeGen
     ( codeGen ) where
 
-import Sprockell
+import Sprockell (Instruction(..))
 import MyParser (Stmt(..), Expr(..))
 import MyCompiler
 
+
+-- -- Entry point
+-- codeGen :: String -> [Instruction]
+-- codeGen stmts = prettyInstructions (compileString "i x; x = 2*3+1; print x;")
+
+-- -- Compile a statement into Sprockell instructions
+-- compileStmt :: Stmt -> [Instruction]
+-- compileStmt stmt = case stmt of
+--     Print e       -> compileExpr e ++ [WriteInstr regA numberIO]
+--     Assign _ _    -> [] -- placeholder
+--     Decl _ _      -> [] -- placeholder
+--     If _ _ _      -> [] -- not implemented yet
+--     While _ _     -> [] -- not implemented yet
+--     ForkJoin _    -> [] -- not implemented yet
+--     Lock _ _      -> [] -- not implemented yet
+--     Block ss      -> concatMap compileStmt ss
+
+-- -- Compile an expression into Sprockell instructions
+-- -- Result will be in regA
+-- compileExpr :: Expr -> [Instruction]
+-- compileExpr expr = case expr of
+--     IntLit n        -> [Load (ImmValue $ fromInteger n) regA]
+--     BinOp "+" l r   -> compileBinary l r Add
+--     BinOp "-" l r   -> compileBinary l r Sub
+--     BinOp "*" l r   -> compileBinary l r Mul
+--     BinOp "/" l r   -> compileBinary l r Div
+--     _               -> [NOP] -- unsupported expr
+
+
+
+prog :: String -> [Instruction]
+prog x = compileString x
+
+
+-- prog :: [Instruction]
+-- prog = [ Load (ImmValue 2) regA
+--         , Load (ImmValue 3) regB
+--         , Compute Mul regA regB regA
+--         , Load (ImmValue 1) regB
+--         , Compute Add regA regB regA
+--         , WriteInstr regA numberIO
+--         , EndProg
+--        ]
 
 -- This is the correct type for integration with Main.hs
 codeGen :: [Stmt] -> [Instruction]
